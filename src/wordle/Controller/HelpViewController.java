9f660f0b3d46f1700c67b519206cddb4d77f2bfb
Umer Ali <umeralikhan403@gmail.com>
2025-05-11 22:17:59 +0100

    added documentation for help view controller

--- src/wordle/Controller/HelpViewController.java
+++ src/wordle/Controller/HelpViewController.java
@@ -13,12 +13,25 @@ import javafx.stage.Stage;
 
 public class HelpViewController {
 
+	/**
+	 * controller for the Help View Screen.
+	 * provides functionality to close the help screen and return to the login view.
+	 */
 	@FXML private Button btnCloseHelp;
 	
+	 /**
+     * Handles the action when the "Close Help" button is clicked.
+     * Navigates the user back to the Login screen.
+     * 
+     * @param e the action event triggered by clicking the button
+     * @throws IOException if the FXML file for the login view cannot be loaded
+     */
 	@FXML
 	private void closeHelpHandler(ActionEvent e) throws IOException {
+		 // load the LoginView FXML
 		Parent registerRoot = FXMLLoader.load(getClass().getResource("/wordle/View/LoginView.fxml"));
-        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        // Get current stage from the event and set the new scene
+		Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         stage.setScene(new Scene(registerRoot));
 	}
 }
