40169355de9c706da1364f00d7fc36a883b95be3
AmeerSH64 <94924474+AmeerSH64@users.noreply.github.com>
2025-05-11 20:46:02 +0100

    Added Documentation to Main Method

--- src/wordle/Main.java
+++ src/wordle/Main.java
@@ -6,7 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
+/**
+ * The Main class loads the application and sets the scene.
+ */
 public class Main extends Application {
+	/**
+	 * The start method is the entry point of the JavaFX application.
+	 * Loads the FXML file and sets it to the scene, specifying the title of the window.
+	 */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/wordle/View/loginView.fxml"));
@@ -17,6 +24,10 @@ public class Main extends Application {
         stage.show();
     }
 
+    /**
+	 * The main method is the entry point of the Java application.
+	 * @param args - command line arguments
+	 */
     public static void main(String[] args) {
         launch();
     }
