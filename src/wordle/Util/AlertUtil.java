3c4a4ea3cd380d9a10d2e579aad394982df263c2
AmeerSH64 <94924474+AmeerSH64@users.noreply.github.com>
2025-05-13 18:55:27 +0100

    Added Documentation to AlertUtil

--- src/wordle/Util/AlertUtil.java
+++ src/wordle/Util/AlertUtil.java
@@ -2,16 +2,38 @@ package wordle.Util;
 
 import javafx.scene.control.Alert;
 
+/**
+ * Utility class for displaying alerts in a JavaFX application.
+ * This class provides methods to show warning and information alerts.
+ */
 public class AlertUtil {
 
+	/**
+	 * Displays a warning alert with the specified title and message.
+	 *
+	 * @param title   The title of the alert.
+	 * @param message The message to be displayed in the alert.
+	 */
     public static void warn(String title, String message) {
         show(Alert.AlertType.WARNING, title, message);
     }
 
+    /**
+     * Displays an information alert with the specified title and message.
+     * @param title   The title of the alert.
+     * @param message The message to be displayed in the alert.
+     */
     public static void info(String title, String message) {
         show(Alert.AlertType.INFORMATION, title, message);
     }
 
+    /**
+	 * Displays an alert of the specified type with the given title and message.
+	 *
+	 * @param type    The type of the alert (e.g., WARNING, INFORMATION).
+	 * @param title   The title of the alert.
+	 * @param message The message to be displayed in the alert.
+	 */
     private static void show(Alert.AlertType type, String title, String message) {
         Alert alert = new Alert(type);
         alert.setTitle(title);
